ecf6532a8cbc126988bba4942ae051c2fd050e0d
Norbert Bonnici <norbert@bonnici.mt>
2026-06-02 20:13:14 +0200

    Initial Commit

---
+++ StrataTests/StrataTests.swift
@@ -0,0 +1,18 @@
+//
+//  StrataTests.swift
+//  StrataTests
+//
+//  Created by Norbert Bonnici on 02/06/2026.
+//
+
+import Testing
+
+struct StrataTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
